2c35ae248f9e9ff9a30b9336f73da40d9e8d061e
Cortezano <christianjakecortezano@gmail.com>
2024-11-10 22:31:54 +0800

    setup arweave wallet code

---
+++ src/main.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ArweaveWalletKit } from "arweave-wallet-kit";
+import { Toaster } from "./components/ui/toaster.tsx";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ArweaveWalletKit
+      config={{
+        permissions: [
+          "ACCESS_ADDRESS",
+          "ACCESS_ALL_ADDRESSES",
+          "ACCESS_PUBLIC_KEY",
+          "SIGNATURE",
+          "SIGN_TRANSACTION",
+        ],
+      }}
+    >
+      <App />
+      <Toaster />
+    </ArweaveWalletKit>
+  </React.StrictMode>
+);
