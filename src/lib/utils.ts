33172b73a22f658b84c7cf5fb5dd106d55377ef0
“Zuaren““ <christianjakecortezano@gmail.com>
2024-11-15 01:04:13 +0800

    UTILS: stored arflip process id here

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+// export const ARFLIP_PROCESS_ID = "XjaI24il_p6DT_ISYn8SWbyptNuCcp0XkxNkyt3hQz8";
+//basically process id use for the backend the process kumbaga
+export const ARFLIP_PROCESS_ID = "ccbHS5mc1kSq8uD09BjtQwyQ19Owji3lw5QWZkCbaPw";
